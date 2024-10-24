96a8db42d67219f02135586b9bc6d14f1711b1ac
prasansha <166462475+ppg14@users.noreply.github.com>
2024-10-25 09:14:19 +0930

    New

--- Prasansha/Coach.cs
+++ Prasansha/Coach.cs
@@ -1,17 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using AnyoneForTennis.Areas.Identity.Data;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace AnyoneForTennis.OldDatas.Models;
+namespace AnyoneForTennis.Models;
 
-public partial class Coach
+public class Coach
 {
+    [Key]
     public int CoachId { get; set; }
-
-    public string FirstName { get; set; } = null!;
-
-    public string LastName { get; set; } = null!;
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
 
     public string? Biography { get; set; }
 
     public byte[]? Photo { get; set; }
+
+    public string? UserId { get; set; }
+
+    [ForeignKey("UserId")]
+    [ValidateNever]
+    public virtual ApplicationUser? User { get; set; }
 }
